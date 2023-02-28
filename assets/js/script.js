8e1634be61c9d81e1e7dbc9683fd967c0c739684
somayehbd <somayehbd@gmail.com>
2023-02-28 23:38:12 +0100

    Added Post request to API

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,42 @@ $('.main-carousel').flickity({
     setGallerySize: false,
     prevNextButtons: false
 });
+
+// send Post request to api
+const LoginForm = document.getElementById('form')
+
+const handleLoginForm = (e) => {
+    e.preventDefault();
+
+    const email = form.email;
+    const password = form.password;
+
+    myHeaders = { "Content-Type": "application/json" };
+
+    let raw = JSON.stringify({
+        "email": email.value,
+        "password": password.value
+    });
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch('http://localhost:3100/register', requestOptions)
+        .then(response => {
+             console.log(response)
+            return response.json()
+        })
+        .then(data => {
+             console.log(data)
+          
+        })
+        .catch(() => { })
+
+
+}
+
+form.addEventListener('submit', handleLoginForm)
